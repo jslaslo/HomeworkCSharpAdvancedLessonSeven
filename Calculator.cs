973def50bdcf9ced2082218d5cc93c756f0b3fcf
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2023-12-18 22:32:03 +0200

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTwo
+{
+    internal class Calculator
+    {
+        public double FirstValue {  get; private set; }
+        public double SecondValue { get; private set; }
+
+        public Calculator(double firstValue, double secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
+        
+        public void Multi()
+        {
+            Console.WriteLine($"Результат умножения: {FirstValue * SecondValue}"); 
+        }
+        public void Division()
+        {
+            Console.WriteLine($"Результат деления: {FirstValue/SecondValue}");
+        }
+        public void Addition()
+        {
+            Console.WriteLine($"Результат сложения: {FirstValue + SecondValue}");
+        }
+        public void Subtrction()
+        {
+            Console.WriteLine($"Результат вычитания: {FirstValue - SecondValue}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+namespace HomeworkTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            double firstValue;
+            double secondValue;
+            while (isWork)
+            {
+                Console.WriteLine("Введите первое число: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+            {
+                    firstValue = value;                    
+                }
+            else
+                {
+                    Console.WriteLine("До свидания!");
+                    return;
+                }
+                Console.WriteLine("Введите второе число: ");
+                if (double.TryParse(Console.ReadLine(), out value))
+                {
+                    secondValue = value;
+                }
+                else
+                {
+                    Console.WriteLine("До свидания!");
+                    return;
+                }
+
+                var calc = new Calculator(firstValue, secondValue);
+                Console.WriteLine("Какую операцию хотите выполнить:" +
+                    "\nУмножить: *; " +
+                    "\nПоделить: /; " +
+                    "\nСложить: +; " +
+                    "\nВычесть: -; ");
+                bool isCorrectOperator = false;
+                while (isCorrectOperator == false)
+                {
+                    switch (Console.ReadLine())
+                    {
+                        case "+":
+                            calc.Addition();
+                            isCorrectOperator = true;
+                            break;
+                        case "-":
+                            calc.Subtrction();
+                            isCorrectOperator = true;
+                            break;
+                        case "*":
+                            calc.Multi();
+                            isCorrectOperator = true;
+                            break;
+                        case "/":
+                            calc.Division();
+                            isCorrectOperator = true;
+                            break;
+                        case "e":
+                            Console.WriteLine("До свидания!");
+                            isCorrectOperator = true;
+                            isWork = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Введедена неверная операция, попробуйте еще раз....");
+                            break;
+                    }
+                }
+               
+            }
+        }
+    }
+}
